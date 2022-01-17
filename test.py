81025b7eb736d6f0d61c7d5e430934bcfcb491c1
Benjamin M. Sainz-Tinajero <bm.sainz@gmail.com>
2022-01-17 11:29:45 -0600

    Update test.py

--- test.py
+++ test.py
@@ -6,13 +6,14 @@ from retr import *
 from gen import *
 
 ds = ['absenteeism-at-work', 'arrhythmia', 'breast-cancer-wisconsin', 'breast-tissue', 'car-evaluation', 'dermatology',
-      'echocardiogram', 'ecoli', 'forest', 'forest-fires', 'german-credit', 'glass', 'hepatitis', 'image-segmentation',
+      'echocardiogram', 'ecoli', 'forest-fires', 'forest', 'german-credit', 'glass', 'hepatitis', 'image-segmentation',
       'ionosphere', 'iris', 'leaf', 'liver', 'parkinsons', 'seeds', 'segment', 'sonar', 'soybean-large',
       'student-performance', 'tic-tac-toe', 'transfusion', 'user-knowledge-modeling', 'wine', 'yeast', 'zoo']
 
-nature = [2, 5, 7, 'canada', 'london', 'parking-lot', 'port-city', 'three', 'seven', 'varadero']
+nature = ['canada', 'coast', 'highway-in-the-desert', 'london', 'parking-lot', 'port-city', 'port', 'road-with-trees', 
+          'varadero', 'white-containers']
 
 if __name__ == "__main__":
-    for d in nature:
+    for d in ds:
         data, n_clusters, X, y, shuffle_index = retrieval(d)
-        f1ecacplus_run(X, n_clusters, data, pop_size=400, max_gens=400, p_crossover=0.95, p_mutation=0.98, test_size=0.75, runs=10, y=y, shuffle_index=shuffle_index)
+        ecacs_run(X, n_clusters, data, pop_size=200, max_gens=200, p_crossover=0.95, p_mutation=0.98, test_size=0.75, runs=10, y=y, shuffle_index=shuffle_index)
