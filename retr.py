f3e8ef3a2680382b3bcf4bebbdbc66c98cfcf30e
Benjamin M. Sainz-Tinajero <bm.sainz@gmail.com>
2022-01-17 18:04:11 -0600

    Update retr.py

--- retr.py
+++ retr.py
@@ -3,6 +3,7 @@ Authors: Benjamin M. Sainz-Tinajero, Andres E. Gutierrez-Rodriguez.
 """
 
 import pandas as pd
+import os
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import AgglomerativeClustering
@@ -11,9 +12,12 @@ def original_data_numerical_dropna(data):
     temp_df = pd.read_csv('data/{}_X.csv'.format(data), header=None)
     for column in temp_df.select_dtypes(include=['object']):
         temp_df[column], _ = pd.factorize(temp_df[column])
-    y_df = pd.read_csv('data/{}_y.csv'.format(data), header=None)
-    temp_df['y'] = y_df[0].to_list()
-    temp_df['y'] = temp_df['y'].astype(str)
+    if os.path.isfile('data/{}_y.csv'.format(data)):
+        y_df = pd.read_csv('data/{}_y.csv'.format(data), header=None)
+        temp_df['y'] = y_df[0].to_list()
+        temp_df['y'] = temp_df['y'].astype(str)
+    else:
+        temp_df['y'] = ['1']*len(temp_df)
     return temp_df
 
 def index_sorting(unsorted_df):
@@ -30,5 +34,8 @@ def retrieval(data):
     sorted_df, n_clusters = index_sorting(unsorted_df)
     shuffle_index = sorted_df.index.to_list()
     X = pd.DataFrame(StandardScaler().fit_transform(sorted_df.iloc[:,:-1]))
-    y = sorted_df.loc[:,'y'].to_list()
+    if os.path.isfile('data/{}_y.csv'.format(data)):
+        y = sorted_df.loc[:,'y'].to_list()
+    else:
+        y = None
     return data, n_clusters, X, y, shuffle_index
