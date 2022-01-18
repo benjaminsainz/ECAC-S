8740915f902ea0a27e2bec87b62ebfd62ebbfe00
Benjamin M. Sainz-Tinajero <bm.sainz@gmail.com>
2022-01-18 11:06:37 -0600

    Update obj.py

--- obj.py
+++ obj.py
@@ -1,5 +1,5 @@
 """
-Authors: Benjamin M. Sainz-Tinajero and Andres E. Gutierrez-Rodriguez @ Tecnologico de Monterrey, 2022.
+Author: Benjamin M. Sainz-Tinajero @ Tecnologico de Monterrey, 2022.
 """
 
 from sklearn.model_selection import train_test_split
