6a920a3931247b0221765be0b024d67644bce136
Benjamin M. Sainz-Tinajero <bm.sainz@gmail.com>
2022-01-18 11:01:53 -0600

    Update retr.py

--- retr.py
+++ retr.py
@@ -1,5 +1,5 @@
 """
-Authors: Benjamin M. Sainz-Tinajero, Andres E. Gutierrez-Rodriguez.
+Authors: Benjamin M. Sainz-Tinajero and Andres E. Gutierrez-Rodriguez @ Tecnologico de Monterrey, 2022.
 """
 
 import pandas as pd
@@ -20,18 +20,19 @@ def original_data_numerical_dropna(data):
         temp_df['y'] = ['1']*len(temp_df)
     return temp_df
 
-def index_sorting(unsorted_df):
+def index_sorting(unsorted_df, n_clusters):
+    if n_clusters == 'auto':
+        n_clusters = len(set(unsorted_df.loc[:,'y'].to_list()))
     X = pd.DataFrame(StandardScaler().fit_transform(unsorted_df.iloc[:,:-1]))
-    n_clusters = len(set(unsorted_df.loc[:,'y'].to_list()))
     model = AgglomerativeClustering(n_clusters, linkage='single', affinity='euclidean').fit(X)
     unsorted_df['clustering_labels'] = model.labels_
     sorted_df = unsorted_df.sort_values(['clustering_labels'])
     sorted_df.drop(['clustering_labels'], axis=1, inplace=True)
     return sorted_df, n_clusters
 
-def retrieval(data):
+def retrieval(data, n_clusters):
     unsorted_df = original_data_numerical_dropna(data)
-    sorted_df, n_clusters = index_sorting(unsorted_df)
+    sorted_df, n_clusters = index_sorting(unsorted_df, n_clusters)
     shuffle_index = sorted_df.index.to_list()
     X = pd.DataFrame(StandardScaler().fit_transform(sorted_df.iloc[:,:-1]))
     if os.path.isfile('data/{}_y.csv'.format(data)):
