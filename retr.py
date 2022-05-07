114f62d81c93b9e67f88879615d0b3607861126c
Benjamin M. Sainz-Tinajero <bm.sainz@gmail.com>
2022-05-07 11:44:31 -0500

    Update retr.py

--- retr.py
+++ retr.py
@@ -2,11 +2,14 @@
 Author: Benjamin M. Sainz-Tinajero @ Tecnologico de Monterrey, 2022.
 """
 
+
 import pandas as pd
 import os
 
+
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import AgglomerativeClustering
+
 
 def original_data_numerical_dropna(data):
     temp_df = pd.read_csv('data/{}_X.csv'.format(data), header=None)
@@ -20,6 +23,7 @@ def original_data_numerical_dropna(data):
         temp_df['y'] = ['1']*len(temp_df)
     return temp_df
 
+
 def index_sorting(unsorted_df, n_clusters):
     if n_clusters == 'auto':
         n_clusters = len(set(unsorted_df.loc[:,'y'].to_list()))
@@ -29,6 +33,7 @@ def index_sorting(unsorted_df, n_clusters):
     sorted_df = unsorted_df.sort_values(['clustering_labels'])
     sorted_df.drop(['clustering_labels'], axis=1, inplace=True)
     return sorted_df, n_clusters
+
 
 def retrieval(data, n_clusters):
     unsorted_df = original_data_numerical_dropna(data)
